51688136f1032c308cbc719933464b2f71983d4a
fdelavega <fdelavega@conwet.com>
2015-12-15 12:21:59 +0100

    Update ServeMedia view to support assets download

--- src/wstore/views.py
+++ src/wstore/views.py
@@ -27,67 +27,34 @@ from django.http import HttpResponse
 
 from store_commons.utils.http import build_response
 from wstore.store_commons.resource import Resource as API_Resource
-from wstore.models import UserProfile, Organization
-from wstore.models import Purchase, Resource, Offering
+from wstore.models import Purchase, Resource
 
 
 class ServeMedia(API_Resource):
 
     def read(self, request, path, name):
-        if request.method != 'GET':
-            return build_response(request, 415, 'Method not supported')
 
         dir_path = os.path.join(settings.MEDIA_ROOT, path)
 
         # Protect the resources from not authorized downloads
-        if dir_path.endswith('resources'):
-            if request.user.is_anonymous():
-                return build_response(request, 401, 'Unauthorized')
+        if dir_path.endswith('assets'):
+            # Retrieve the given digital asset
+            try:
+                asset = Resource.objects.get(resource_path=path + '/' + name)
+            except:
+                return build_response(request, 404, 'The specified asset does not exists')
 
-            # Check if the request user has access to the resource
-            splited_name = name.split('__')
-            prov = Organization.objects.get(name=splited_name[0])
-            resource = Resource.objects.get(provider=prov, name=splited_name[1], version=splited_name[2])
+            # Check if the user has permissions to download the asset
+            if not asset.is_public and (request.user.is_anonymous() or
+                    request.user.current_organization != asset.provider):
 
-            if not resource.open:
-                user_profile = UserProfile.objects.get(user=request.user)
-                found = False
-
-                # Check if the user has purchased an offering with the resource 
-                # only if the offering is not open
-            
-                for off in user_profile.offerings_purchased:
-                    o = Offering.objects.get(pk=off)
-
-                    for res in o.resources:
-                        if str(res) == resource.pk:
-                            found = True
-                            break
-
-                    if found:
-                        break
-
-                if not found:
-                    # Check if the user organization has an offering with the resource
-                    for off in user_profile.current_organization.offerings_purchased:
-                        o = Offering.objects.get(pk=off)
-
-                        for res in o.resources:
-                            if str(res) == resource.pk:
-                                found = True
-                                break
-
-                        if found:
-                            break
-
-                    if not found:
-                        return build_response(request, 404, 'Not found')
+                return build_response(request, 403, 'Your are not authorized to download the specified asset')
 
         if dir_path.endswith('bills'):
             if request.user.is_anonymous():
-                return build_response(request, 401, 'Unauthorized')
+                return build_response(request, 401, 'You must provide credentials for downloading invoices')
 
-            user_profile = UserProfile.objects.get(user=request.user)
+            user_profile = request.user.userprofile
             purchase = Purchase.objects.get(ref=name[:24])
 
             if purchase.organization_owned:
