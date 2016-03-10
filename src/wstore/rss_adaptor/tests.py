b693b29c771dce9cf7cced93c00279e22d608ac9
fdelavega <fdelavega@conwet.com>
2016-03-10 15:36:15 +0100

    Update RSSAdaptor tests

--- src/wstore/rss_adaptor/tests.py
+++ src/wstore/rss_adaptor/tests.py
@@ -28,96 +28,27 @@ from django.conf import settings
 from wstore.rss_adaptor import rss_adaptor, rss_manager, model_manager
 
 
-__test__ = False
-
-
-class FakeUrlib2Rss():
-
-    _opener = None
-
-    def __init__(self):
-        self._opener = self.Opener(self.Response())
-
-    def build_opener(self):
-        return self._opener
-
-    class Opener():
-
-        _response = None
-        _method = None
-        _header = None
-        _body = None
-        _url = None
-
-        def __init__(self, response):
-            self._response = response
-
-        def open(self, request):
-            self._method = request.get_method()
-            self._header = request.get_header('Content-type')
-            self._body = request.data
-            self._url = request.get_full_url()
-
-            return self._response
-
-    class Response():
-        url = 'http://response.com/'
-        code = 201
-
-
 class RSSAdaptorTestCase(TestCase):
 
-    tags = ('fiware-ut-18',)
-    
+    tags = ('rss-adaptor',)
+
+    def setUp(self):
+        settings.WSTOREMAIL = 'testmail@mail.com'
+        settings.RSS = 'http://testhost.com/rssHost/'
+        settings.STORE_NAME = 'wstore'
+
+        rss_adaptor.requests = MagicMock()
+        self._response = MagicMock()
+        rss_adaptor.requests.post.return_value = self._response
+
     def test_rss_client(self):
+        # Create mocks
+        self._response.status_code = 201
 
-        expected_xml = """
-        <?xmlversion='1.0'encoding='ASCII'?>
-        <cdrs>
-            <cdr>
-                <id_service_provider>test_provider</id_service_provider>
-                <id_application>test_service</id_application>
-                <id_event>Subscription event</id_event>
-                <id_correlation>2</id_correlation>
-                <purchase_code>1234567890</purchase_code>
-                <parent_app_id>test_offering</parent_app_id>
-                <product_class>SaaS</product_class>
-                <description>The description</description>
-                <cost_currency>EUR</cost_currency>
-                <cost_units>10</cost_units>
-                <tax_currency>EUR</tax_currency>
-                <tax_units>0.0</tax_units>
-                <cdr_source>WStore</cdr_source>
-                <id_operator>1</id_operator>
-                <id_country>SP</id_country>
-                <time_stamp>10-05-13 10:00:00</time_stamp>
-                <id_user>test_customer</id_user>
-            </cdr>
-            <cdr>
-                <id_service_provider>test_provider</id_service_provider>
-                <id_application>test_service</id_application>
-                <id_event>Pay per use event</id_event>
-                <id_correlation>3</id_correlation>
-                <purchase_code>1234567890</purchase_code>
-                <parent_app_id>test_offering</parent_app_id>
-                <product_class>SaaS</product_class>
-                <description>The description</description>
-                <cost_currency>EUR</cost_currency>
-                <cost_units>1</cost_units>
-                <tax_currency>EUR</tax_currency>
-                <tax_units>0.0</tax_units>
-                <cdr_source>WStore</cdr_source>
-                <id_operator>1</id_operator>
-                <id_country>SP</id_country>
-                <time_stamp>10-05-13 10:00:00</time_stamp>
-                <id_user>test_customer</id_user>
-            </cdr>
-        </cdrs>"""
+        rss_ad = rss_adaptor.RSSAdaptor()
 
-        cdr = [{
+        rss_ad.send_cdr([{
             'provider': 'test_provider',
-            'service': 'test_service',
-            'defined_model': 'Subscription event',
             'correlation': '2',
             'purchase': '1234567890',
             'offering': 'test_offering',
@@ -125,53 +56,39 @@ class RSSAdaptorTestCase(TestCase):
             'description': 'The description',
             'cost_currency': 'EUR',
             'cost_value': '10',
-            'tax_currency': 'EUR',
             'tax_value': '0.0',
-            'source': 'WStore',
-            'operator': '1',
-            'country': 'SP',
             'time_stamp': '10-05-13 10:00:00',
             'customer': 'test_customer',
-        }, {
-            'provider': 'test_provider',
-            'service': 'test_service',
-            'defined_model': 'Pay per use event',
-            'correlation': '3',
-            'purchase': '1234567890',
-            'offering': 'test_offering',
-            'product_class': 'SaaS',
-            'description': 'The description',
-            'cost_currency': 'EUR',
-            'cost_value': '1',
-            'tax_currency': 'EUR',
-            'tax_value': '0.0',
-            'source': 'WStore',
-            'operator': '1',
-            'country': 'SP',
-            'time_stamp': '10-05-13 10:00:00',
-            'customer': 'test_customer',
-        }]
+            'event': 'One time'
+        }])
 
-        fake_urllib2 = FakeUrlib2Rss()
-        rss_adaptor.urllib2 = fake_urllib2
+        rss_adaptor.requests.post.assert_called_once_with(
+            'http://testhost.com/rssHost/rss/cdrs', json=[{
+                'cdrSource': 'testmail@mail.com',
+                'productClass': 'SaaS',
+                'correlationNumber': '2',
+                'timestamp': '10-05-13T10:00:00Z',
+                'application': 'test_offering',
+                'transactionType': 'C',
+                'event': 'One time',
+                'referenceCode': '1234567890',
+                'description': 'The description',
+                'chargedAmount': '10',
+                'chargedTaxAmount': '0.0',
+                'currency': 'EUR',
+                'customerId': 'test_customer',
+                'appProvider': 'test_provider'
+            }],
+            headers={
+                'content-type': 'application/json',
+                'X-Actor-ID': 'wstore',
+                'X-Roles': 'provider',
+                'X-Email': 'testmail@mail.com'
+            })
 
-        rss = MagicMock()
-        rss.host = 'http://examplerss/fiware_rss/'
-        rss.access_token = 'accesstoken'
-        rss_adap = rss_adaptor.RSSAdaptorV1(rss)
-        rss_adap.send_cdr(cdr)
-
-        opener = fake_urllib2._opener
-        self.assertEqual(opener._method, 'POST')
-        self.assertEqual(opener._header, 'application/xml')
-        self.assertEqual(opener._url, 'http://examplerss/fiware_rss/fiware-rss/rss/cdrs')
-
-        expected_xml = expected_xml.replace(' ', '')
-        expected_xml = expected_xml.replace('\n', '')
-
-        body = opener._body.replace(' ', '')
-        body = body.replace('\n', '')
-        self.assertEqual(expected_xml, body)
+    def test_rss_remote_error(self):
+        # Create Mocks
+        self._response.status_code = 500
 
 
 class ModelManagerTestCase(TestCase):
