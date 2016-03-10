e729ff945e57335c034dc97e330fdbec7e9c3549
fdelavega <fdelavega@conwet.com>
2016-03-10 16:31:31 +0100

    Update RS Models client tests

--- src/wstore/rss_adaptor/tests.py
+++ src/wstore/rss_adaptor/tests.py
@@ -91,122 +91,126 @@ class RSSAdaptorTestCase(TestCase):
         self._response.status_code = 500
 
 
+BASIC_MODEL = {
+    'ownerProviderId': 'provider',
+    'ownerValue': 70,
+    'aggregatorValue': 30,
+    'productClass': 'class'
+}
+
+ST_MODEL = {
+    'ownerProviderId': 'provider',
+    'ownerValue': 70,
+    'aggregatorValue': 30,
+    'productClass': 'class',
+    'stakeholders': []
+}
+
+MISSING_OWNER_VAL = {
+    'ownerProviderId': 'provider',
+    'aggregatorValue': 30,
+    'productClass': 'class'
+}
+
+INV_OWNER_VAL = {
+    'ownerProviderId': 'provider',
+    'ownerValue': 'invalid',
+    'aggregatorValue': 30,
+    'productClass': 'class'
+}
+
+INV_AGG_VAL = {
+    'ownerProviderId': 'provider',
+    'ownerValue': 70,
+    'aggregatorValue': 'invalid',
+    'productClass': 'class'
+}
+
+MISSING_AGG_VAL = {
+    'ownerProviderId': 'provider',
+    'ownerValue': 70,
+    'productClass': 'class'
+}
+
+MISSING_PROV = {
+    'ownerValue': 70,
+    'aggregatorValue': 30,
+    'productClass': 'class'
+}
+
+INV_PROV = {
+    'ownerProviderId': 20,
+    'ownerValue': 70,
+    'aggregatorValue': 30,
+    'productClass': 'class'
+}
+
+MISSING_CLASS = {
+    'ownerProviderId': 'provider',
+    'ownerValue': 70,
+    'aggregatorValue': 30,
+}
+
+INV_CLASS = {
+    'ownerProviderId': 'provider',
+    'ownerValue': 70,
+    'aggregatorValue': 30,
+    'productClass': 20
+}
+
+EXP_BASIC_MODEL = {
+    'aggregatorId': 'testmail@mail.com',
+    'ownerProviderId': 'provider',
+    'ownerValue': '70',
+    'aggregatorValue': '30',
+    'productClass': 'class',
+    'algorithmType': 'FIXED_PERCENTAGE',
+    'stakeholders': []
+}
+
+
 class ModelManagerTestCase(TestCase):
 
     tags = ('rs-models', )
 
     @classmethod
-    def setUpClass(cls):
-        # Save used libraries
-        cls._old_RSS = rss_manager.RSS
-
-        # Create Mocks
-        cls.rss_mock = MagicMock()
-        cls.opener = MagicMock()
-        cls.mock_response = MagicMock()
-        cls.opener.open.return_value = cls.mock_response
-
-        rss_manager.RSS = MagicMock()
-        rss_manager.RSS.objects.get.return_value = cls.rss_mock
-        super(ModelManagerTestCase, cls).setUpClass()
-
-    @classmethod
     def tearDownClass(cls):
-        rss_manager.RSS = cls._old_RSS
         reload(rss_manager)
         super(ModelManagerTestCase, cls).tearDownClass()
 
     def setUp(self):
-        self.rss_mock.reset_mock()
-        self.rss_mock.host = 'http://testrss.com/'
-        self.manager = model_manager.ModelManagerV1(self.rss_mock, 'accesstoken')
+        settings.WSTOREMAIL = 'testmail@mail.com'
+        settings.RSS = 'http://testhost.com/rssHost/'
+        settings.STORE_NAME = 'wstore'
+
+        # Create Mocks
+        self.manager = model_manager.ModelManager({})
         self.manager._make_request = MagicMock()
         TestCase.setUp(self)
 
     @parameterized.expand([
-        ('complete_model', {
-            'class': 'app',
-            'percentage': 20.0
-        }),
-        ('complete_provider',  {
-            'class': 'app',
-            'percentage': 20.0
-        }, 'test_user'),
-        ('missing_class', {
-            'percentage': 20.0
-        }, None, ValueError, 'Missing a required field in model info'),
-        ('missing_perc', {
-            'class': 'app'
-        }, None, ValueError, 'Missing a required field in model info'),
-        ('inv_data', ('app', 20.0), None, TypeError, 'Invalid type for model info'),
-        ('inv_class', {
-            'class': 7,
-            'percentage': 20.0
-        }, None, TypeError, 'Invalid type for class field'),
-        ('inv_percentage', {
-            'class': 'app',
-            'percentage': '20.0'
-        }, None, TypeError, 'Invalid type for percentage field'),
-        ('bigger_perc', {
-            'class': 'app',
-            'percentage': 102.0
-        }, None, ValueError, 'The percentage must be a number between 0 and 100')
+        ('correct', BASIC_MODEL, EXP_BASIC_MODEL),
+        ('with_stakeholders', ST_MODEL, EXP_BASIC_MODEL),
+        ('missing_owner_value', MISSING_OWNER_VAL, None, ValueError, 'Missing a required field in model info: ownerValue'),
+        ('invalid_owner_value', INV_OWNER_VAL, None, TypeError, 'Invalid type for ownerValue field'),
+        ('missing_aggregator_value', MISSING_AGG_VAL, None, ValueError, 'Missing a required field in model info: aggregatorValue'),
+        ('invalid_aggregator_value', INV_AGG_VAL, None, TypeError, 'Invalid type for aggregatorValue field'),
+        ('missing_provider', MISSING_PROV, None, ValueError, 'Missing a required field in model info: ownerProviderId'),
+        ('invalid_provider_type', INV_PROV, None, TypeError, 'Invalid type for ownerProviderId field'),
+        ('missing_product_class', MISSING_CLASS, None, ValueError, 'Missing a required field in model info: productClass'),
+        ('invalid_product_class_type', INV_CLASS, None, TypeError, 'Invalid type for productClass field')
     ])
-    def test_create_model(self, name, data, provider=None, err_type=None, err_msg=None):
+    def test_create_model(self, name, data, exp_data, err_type=None, err_msg=None):
 
         error = None
         try:
-            self.manager.create_revenue_model(data, provider)
+            self.manager.create_revenue_model(data)
         except Exception as e:
             error = e
 
-        if not err_type:
-            self.assertEquals(error, None)
-            # Check calls
-            if provider:
-                exp_prov = provider
-            else:
-                exp_prov = settings.STORE_NAME.lower() + '-provider'
-
-            exp_data = {
-                'appProviderId': exp_prov,
-                'productClass': data['class'],
-                'percRevenueShare': data['percentage']
-            }
-            self.manager._make_request.assert_called_with('POST', 'http://testrss.com/fiware-rss/rss/rsModelsMgmt', exp_data)
+        if err_type is None:
+            self.assertTrue(error is None)
+            self.manager._make_request.assert_called_once('POST', 'http://testhost.com/rssHost/rss/models', exp_data)
         else:
-            self.assertTrue(isinstance(error, err_type))
-            self.assertEquals(unicode(e), err_msg)
-
-    @parameterized.expand([
-        ('default_prov',),
-        ('provider', 'test_user')
-    ])
-    def test_get_model(self, name, provider=None):
-
-        mock_models = [{
-            'appProviderId': 'wstore',
-            'productClass': 'app',
-            'percRevenueShare': 20.0
-        }]
-        self.manager._make_request.return_value = mock_models
-
-        # Call the get method
-        error = False
-        try:
-            models = self.manager.get_revenue_models(provider)
-        except:
-            error = True
-
-        # Check no error occurs
-        self.assertFalse(error)
-
-        # Check calls
-        if not provider:
-            provider = settings.STORE_NAME.lower() + '-provider'
-
-        from urllib import quote
-        self.manager._make_request.assert_called_once_with('GET', 'http://testrss.com/fiware-rss/rss/rsModelsMgmt?appProviderId=' + quote(provider))
-
-        # Check returned value
-        self.assertEquals(models, mock_models)
+            self.assertTrue(isinstance(e, err_type))
+            self.assertEquals(err_msg, unicode(e))
