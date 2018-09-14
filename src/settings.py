67026e13c4d89f93e49a999df49f2efc4436330a
Francisco de la Vega <fdelavega@conwet.com>
2018-09-14 13:31:11 +0200

    Allow to configure the system using environment variables

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2013 - 2017 CoNWeT Lab., Universidad Politécnica de Madrid
+# Copyright (c) 2013 - 2018 CoNWeT Lab., Universidad Politécnica de Madrid
 
 # This file belongs to the business-charging-backend
 # of the Business API Ecosystem.
@@ -20,10 +20,12 @@
 
 from __future__ import unicode_literals
 
-from os import path
+from os import path, environ
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+
+ALLOWED_HOSTS = ['*']
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -190,9 +192,44 @@ CLIENTS = {
     None: 'wstore.charging_engine.payment_client.payment_client.PaymentClient'
 }
 
-PAYMENT_CLIENT = CLIENTS[PAYMENT_METHOD]
-
 NOTIF_CERT_FILE = None
 NOTIF_CERT_KEY_FILE = None
 
 from services_settings import *
+
+# =====================================
+# ENVIRONMENT SETTINGS
+# =====================================
+
+DATABASES['default']['NAME'] = environ.get('BAE_CB_MONGO_DB', DATABASES['default']['NAME'])
+DATABASES['default']['USER'] = environ.get('BAE_CB_MONGO_USER', DATABASES['default']['USER'])
+DATABASES['default']['PASSWORD'] = environ.get('BAE_CB_MONGO_PASS', DATABASES['default']['PASSWORD'])
+DATABASES['default']['HOST'] = environ.get('BAE_CB_MONGO_SERVER', DATABASES['default']['HOST'])
+DATABASES['default']['PORT'] = environ.get('BAE_CB_MONGO_PORT', DATABASES['default']['PORT'])
+
+ADMIN_ROLE = environ.get('BAE_LP_OAUTH2_ADMIN_ROLE', ADMIN_ROLE)
+PROVIDER_ROLE = environ.get('BAE_LP_OAUTH2_SELLER_ROLE', PROVIDER_ROLE)
+CUSTOMER_ROLE = environ.get('BAE_LP_OAUTH2_CUSTOMER_ROLE', CUSTOMER_ROLE)
+
+WSTOREMAILUSER = environ.get('BAE_CB_EMAIL_USER', WSTOREMAILUSER)
+WSTOREMAIL = environ.get('BAE_CB_EMAIL', WSTOREMAIL)
+WSTOREMAILPASS = environ.get('BAE_CB_EMAIL_PASS', WSTOREMAILPASS)
+SMTPSERVER = environ.get('BAE_CB_EMAIL_SMTP_SERVER', SMTPSERVER)
+SMTPPORT = environ.get('BAE_CB_EMAIL_SMTP_PORT', SMTPPORT)
+
+PAYMENT_METHOD = environ.get('BAE_CB_PAYMENT_METHOD', PAYMENT_METHOD)
+
+VERIFY_REQUESTS = environ.get('BAE_CB_VERIFY_REQUESTS', VERIFY_REQUESTS)
+
+SITE = environ.get('BAE_SERVICE_HOST', SITE)
+LOCAL_SITE = environ.get('BAE_CB_LOCAL_SITE', LOCAL_SITE)
+
+CATALOG = environ.get('BAE_CB_CATALOG', CATALOG)
+INVENTORY = environ.get('BAE_CB_INVENTORY', INVENTORY)
+ORDERING = environ.get('BAE_CB_ORDERING', ORDERING)
+BILLING = environ.get('BAE_CB_BILLING', BILLING)
+RSS = environ.get('BAE_CB_RSS', RSS)
+USAGE = environ.get('BAE_CB_LOCAL_SITE', USAGE)
+AUTHORIZE_SERVICE = environ.get('BAE_CB_AUTHORIZE_SERVICE', AUTHORIZE_SERVICE)
+
+PAYMENT_CLIENT = CLIENTS[PAYMENT_METHOD]
