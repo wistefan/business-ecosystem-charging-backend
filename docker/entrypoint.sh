cae7175dcecedfa676500c8a5f928005d608822b
Stefan Wiedemann <wistefan@googlemail.com>
2022-12-22 14:35:30 +0100

    use conn string

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -128,7 +128,7 @@ done
 echo "Starting charging server"
 
 python3 manage.py migrate
-echo Mongo
-echo mongodb-migrate --host ${MONGO_HOST} --port ${MONGO_PORT} --database ${BAE_CB_MONGO_DB} --username ${BAE_CB_MONGO_USER} --password ${BAE_CB_MONGO_PASS} --migrations migrations
-mongodb-migrate --host ${MONGO_HOST} --port ${MONGO_PORT} --database ${BAE_CB_MONGO_DB} --username ${BAE_CB_MONGO_USER} --password ${BAE_CB_MONGO_PASS} --migrations migrations
+
+mongodb-migrate --url mongodb://${BAE_CB_MONGO_USER}:${BAE_CB_MONGO_PASS}@${MONGO_HOST}:27017/${BAE_CB_MONGO_DB} --migrations migrations
+
 gunicorn wsgi:application --workers 1 --forwarded-allow-ips "*" --log-file - --bind 0.0.0.0:8006 --log-level ${LOGLEVEL}
